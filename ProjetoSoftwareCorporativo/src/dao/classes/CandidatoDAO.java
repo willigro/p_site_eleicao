3b16d0e7d1ab8c04de0bfa30ef7383d3b6d65d5b
willigro <willi.gro@hotmail.com>
2016-10-16 14:56:41 -0200

    Corrigindo o metodo consultarCandidato

--- ProjetoSoftwareCorporativo/src/dao/classes/CandidatoDAO.java
+++ ProjetoSoftwareCorporativo/src/dao/classes/CandidatoDAO.java
@@ -10,10 +10,13 @@ import dao.interfaces.ICandidatoDAO;
 public class CandidatoDAO extends DAOGenerico<Candidato> implements ICandidatoDAO {
 
 	@Override
-	public List<Candidato> consultarCandidatos() {
+	public List<Candidato> consultarCandidatos() throws Exception{
 		List<Candidato> lista_candidatos = new ArrayList<>();
 		lista_candidatos = getManager().createQuery("SELECT"
 				+ "cand FROM Candidato cand").getResultList();
+		if(lista_candidatos.isEmpty()){
+			throw new Exception("Não há registros armazenados");
+		}
 		return lista_candidatos;
 	}
 
--- ProjetoSoftwareCorporativo/src/dao/interfaces/ICandidatoDAO.java
+++ ProjetoSoftwareCorporativo/src/dao/interfaces/ICandidatoDAO.java
@@ -6,7 +6,7 @@ import classesBasicas.Candidato;;
 public interface ICandidatoDAO {
 
 	public void insert(Candidato candidato) throws Exception;
-	public List<Candidato> consultarCandidatos();
+	public List<Candidato> consultarCandidatos() throws Exception;
 	public void cadastrarCandidato(Candidato candidato) throws Exception;
 	public void removerCandidato(Candidato candidato) throws Exception;
 	public Candidato alterarCandidato(Candidato candidato) throws Exception;
