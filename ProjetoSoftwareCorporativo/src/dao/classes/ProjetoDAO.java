8fdacec48d960b8f698e4a5b24fde51fe00066c0
willigro <willi.gro@hotmail.com>
2016-10-19 00:52:48 -0200

    Ajustando consultaProjeto e add classe ControllerProjeto

--- ProjetoSoftwareCorporativo/src/dao/classes/ProjetoDAO.java
+++ ProjetoSoftwareCorporativo/src/dao/classes/ProjetoDAO.java
@@ -14,11 +14,19 @@ public class ProjetoDAO extends DAOGenerico<Projeto> implements IProjetoDAO {
 
 	@Override
 	public List<Projeto> consultarProjetosFiltrados(Projeto projeto) throws Exception {		
-		String montagemQuery = "SELECT proj FROM Projeto proj WHERE id_cand = id_cand";
+		String montagemQuery = "SELECT proj FROM Projeto proj WHERE id_proj = id_proj";
 		Query query = null;
 		try{
 			if(projeto.getTitulo_proj() != null || !projeto.getTitulo_proj().trim().equals("")){
 				montagemQuery += " AND proj.titulo_proj = :titulo_proj";
+			}
+			if(projeto.getCanditado_proj().getNome_cand() != null || !projeto.getCanditado_proj().getNome_cand().trim().equals("")){
+				//montagemQuery += " AND proj.titulo_proj = :titulo_proj";
+				throw new Exception("Implementar getCandidato NOME");
+			}
+			if(projeto.getCanditado_proj().getId_cand() > 0){
+				//montagemQuery += " AND proj.titulo_proj = :titulo_proj";
+				throw new Exception("Implementar getCandidato ID");	
 			}
 			if(projeto.getId_proj() > 0){
 				montagemQuery += " AND proj.id_proj = :id_proj";
