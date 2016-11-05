7f983eab37226b8c7e61b3717d3cc617c1572103
GlauberUnibratec <glauberhot7@hotmail.com>
2016-11-04 21:32:08 -0300

    tela parcialmete concluida de cadastro do candidato

--- ProjetoEleicao/src/dao/classes/CandidatoDAO.java
+++ ProjetoEleicao/src/dao/classes/CandidatoDAO.java
@@ -31,37 +31,37 @@ public class CandidatoDAO extends DAOGenerico<Candidato> implements ICandidatoDA
 	public List<Candidato> consultarCandidatosFiltrados(Candidato candidato) throws Exception {
 		String montagemQuery = "SELECT cand FROM Candidato cand WHERE id_cand = id_cand";
 		Query query = getManager().createQuery(montagemQuery);
-		
+
 		try {
 			if (candidato.getNome_cand() != null || !candidato.getNome_cand().trim().equals("")) {
 				montagemQuery += " AND cand.nome_cand = :nome_cand";
 				query.setParameter("nome_cand", candidato.getNome_cand());
 			}
 			if (candidato.getId_cand() > 0) {
-				montagemQuery += " AND cand.id_cand = :id_cand";				
+				montagemQuery += " AND cand.id_cand = :id_cand";
 			}
 			if (candidato.getCidade_cand() != null || !candidato.getCidade_cand().getNome_cid().trim().equals("")) {
-				montagemQuery += " AND cand.cidade_cand = :cidade_cand";				
+				montagemQuery += " AND cand.cidade_cand = :cidade_cand";
 			}
 			if (candidato.getEstado_cand() != null || !candidato.getEstado_cand().getNome_est().trim().equals("")) {
-				montagemQuery += " AND cand.estado_cand = :estado_cand";				
+				montagemQuery += " AND cand.estado_cand = :estado_cand";
 			}
 			if (candidato.getPartido_cand() != null || !candidato.getPartido_cand().getNome_part().trim().equals("")) {
 				montagemQuery += " AND cand.partido_cand = :partido_cand";
 			}
 			if (candidato.getNumero_cand() > 0) {
-				montagemQuery += " AND cand.numero_cand = :numero_cand";				
+				montagemQuery += " AND cand.numero_cand = :numero_cand";
 			}
 			if (candidato.getTipo_Cargo_cand() != null || !candidato.getTipo_Cargo_cand().trim().equals("")) {
 				montagemQuery += " AND cand.tipo_Cargo_cand = :tipo_Cargo_cand";
 			}
-			
+
 			query = getManager().createQuery(montagemQuery);
-			
+
 			if (query.getResultList().isEmpty()) {
 				throw new Exception("Não há registros armazenados");
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception(e.getMessage());
@@ -83,12 +83,13 @@ public class CandidatoDAO extends DAOGenerico<Candidato> implements ICandidatoDA
 	public void cadastrarCandidato(Candidato candidato) throws Exception {
 		super.insert(candidato);
 	}
-	
-	public boolean retornaNumero(Candidato candidato) throws Exception {
-		Query query = super.getManager().createQuery("SELECT u FROM Candidato u WHERE Numero_cand = :Numero AND id_cid = :Cidade", Candidato.class);
-		query.setParameter("Numero", candidato.getNumero_cand());
-		query.setParameter("Cidade", candidato.getCidade_cand().getId_cid());
-		return query.getResultList().isEmpty();
-	}
 
+	public boolean retornaNumero(Candidato candidato) throws Exception {
+			Query query = super.getManager().createQuery(
+					"SELECT u FROM Candidato u WHERE Numero_cand = :Numero AND id_cid = :Cidade", Candidato.class);
+			query.setParameter("Numero", candidato.getNumero_cand());
+			query.setParameter("Cidade", candidato.getCidade_cand().getId_cid());
+			return query.getResultList().isEmpty();	
+		}
 }
+
