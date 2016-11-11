cc1c761e29107f59977c0d490881cb5f96fa258d
willigro <willi.gro@hotmail.com>
2016-11-11 19:04:57 -0200

    Uffa.. o negocio foi louco, add prime 6.0 e ajustes na tela de consulta, add navMenu

--- ProjetoEleicao/src/beans/CandidatoBean.java
+++ ProjetoEleicao/src/beans/CandidatoBean.java
@@ -48,6 +48,13 @@ public class CandidatoBean {
 	public String remover() throws Exception {
 		return "removerCandidato";
 	}
+	
+	/**
+	 * Utiliza a funcao de filtragem na consulta para tornar a navegacao mais dinamica
+	 * */
+	public String pagConsultarCandidato(){
+		return "consultaCandidatos";
+	}
 
 	public String cadastrar() throws Exception {
 		try {
