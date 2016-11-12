9e5f38019adf5d9eecda1b6db42c960183388c5d
ErickdaCosta <erick-548@hotmail.com>
2016-11-12 20:02:54 -0300

    aperfeicoamente estetico da tela de cadastro de usuario

--- ProjetoEleicao/src/beans/UsuarioBean.java
+++ ProjetoEleicao/src/beans/UsuarioBean.java
@@ -1,16 +1,19 @@
 package beans;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.application.FacesMessage;
 import classesBasicas.Usuario;
 import facade.Facade;
 
 @ManagedBean()
 public class UsuarioBean {
-	
+
 	private Usuario usuario;
 	private Facade fachada;
-	
-	public UsuarioBean(){
+
+	public UsuarioBean() {
 		this.usuario = new Usuario();
 		this.fachada = new Facade();
 	}
@@ -22,11 +25,30 @@ public class UsuarioBean {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
-	public String cadastrarUsuario() throws Exception{
-		usuario.setAtivo_user(true);
-		this.fachada.cadastrarUsuario(usuario);
+
+	public void buttonAction(ActionEvent actionEvent) {
+		try {
+			usuario.setAtivo_user(true);
+			this.fachada.cadastrarUsuario(usuario);
+			addMensagem("Cadastrado com Sucesso!");
+		} catch (Exception e) {
+			addMensagem(e.getMessage());
+		}
+	}
+
+	private void addMensagem(String texto) {
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO, texto, null);
+		FacesContext.getCurrentInstance().addMessage(null, mensagem);
+	}
+
+	public String cadastrarUsuario() {
+		try {
+			usuario.setAtivo_user(true);
+			this.fachada.cadastrarUsuario(usuario);
+			addMensagem("Cadastrado com Sucesso!");
+		} catch (Exception e) {
+			addMensagem(e.getMessage());
+		}
 		return "";
 	}
 }
-
