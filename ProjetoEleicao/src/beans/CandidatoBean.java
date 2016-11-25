512141c8f2d7129ec5776ebb015ad85182b16ef9
GlauberUnibratec <glauberhot7@hotmail.com>
2016-11-25 20:18:52 -0300

    conflitinhos bestas

--- ProjetoEleicao/src/beans/CandidatoBean.java
+++ ProjetoEleicao/src/beans/CandidatoBean.java
@@ -86,19 +86,13 @@ public class CandidatoBean {
 	public String pagEditarCandidato() {
 		return "paginaEditarCand";
 	}
-	
-	public String pagAdmin(){
+
+	public String pagAdmin() {
 		return "paginaAdmin";
 	}
 
 	public String cadastrar() throws Exception {
 		try {
-<<<<<<< HEAD
-			this.candidato.getCidade_cand().setId_cid(1);
-			// this.candidato.getEstado_cand().setId_est(this.idEstado);
-			this.candidato.getPartido_cand().setId_part(1);
-=======
->>>>>>> a395f87f943ef68ee33e04b6a8046309f076cd78
 			fachada.cadastrarCandidato(candidato);
 			addMensagem("Cadastrado com Sucesso!");
 		} catch (Exception e) {
@@ -123,7 +117,7 @@ public class CandidatoBean {
 			this.candidato.getEstado_cand().setId_est(idEstado);
 			System.out.println(this.candidato.toString());
 			this.candidatos = fachada.consultarCandidatosFiltrados(this.candidato);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
