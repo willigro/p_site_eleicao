6d98d9b114d639b46af674771f82ac0dadb8174d
Jaime <jaime_t.t@hotmail.com>
2016-11-26 17:23:24 -0200

    Adicionando classe CandidatoBean, n sei por que o bug

--- ProjetoEleicao/src/beans/CandidatoBean.java
+++ ProjetoEleicao/src/beans/CandidatoBean.java
@@ -14,6 +14,7 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 import classesBasicas.Candidato;
+import classesBasicas.Cargo;
 import classesBasicas.Cidade;
 import classesBasicas.Estado;
 import classesBasicas.Partido;
