00e37a4be580d2872c27b4d5c67a1d832b23a902
willigro <willi.gro@hotmail.com>
2016-11-25 23:37:21 -0200

    Tela de candidato selecionado e cargo que n quis ir

---
+++ ProjetoEleicao/src/classesBasicas/Cargo.java
@@ -0,0 +1,22 @@
+package classesBasicas;
+
+public class Cargo {
+	private int id_cargo;
+	private String nome_cargo;
+	public Cargo() {
+	
+	}
+	public int getId_cargo() {
+		return id_cargo;
+	}
+	public void setId_cargo(int id_cargo) {
+		this.id_cargo = id_cargo;
+	}
+	public String getNome_cargo() {
+		return nome_cargo;
+	}
+	public void setNome_cargo(String nome_cargo) {
+		this.nome_cargo = nome_cargo;
+	}
+
+}
